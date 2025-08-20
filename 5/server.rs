e3d0758b8542589a553188fe1c72e6bab80e8383
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-20 00:25:04 -0700

    handle client in a dedicated function

--- 5/server.rs
+++ 5/server.rs
@@ -1,20 +1,24 @@
+use std::io;
+use std::net::TcpStream;
 use std::io::Write;
 
-fn main() -> Result<(), std::io::Error> {
+fn handle_client(mut stream: TcpStream) -> Result<(), io::Error> {
+    println!("connected to client");
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = status_line.as_bytes();
+    stream.write_all(response)?;
+    println!("sent to client: {}", status_line);
+    Ok(())
+}
+
+fn main() -> Result<(), io::Error> {
     let ipaddr = "127.0.0.1";
     let port = "4242";
     let socket = format!("{}:{}", ipaddr, port);
     let listener = std::net::TcpListener::bind(socket)?;
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("connected to client");
-                let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-                let response = status_line.as_bytes();
-                stream.write_all(response)?;
-                stream.flush()?;
-                println!("sent to client: {}", status_line);
-            }
+            Ok(stream) => handle_client(stream)?,
             Err(e) => {
                 println!("failed to connect to client. {}", e);
                 return Err(e);
